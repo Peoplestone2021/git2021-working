2db4225a7258be077e7a14de46e174f0b311c531
YANG MIN SEOK <peoplestone2021@gmail.com>
2021-08-20 02:16:23 +0900

    AccountManager

--- 4-react/myworkspace/src/components/AccountManager.tsx
+++ 4-react/myworkspace/src/components/AccountManager.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { sortAndDeduplicateDiagnostics } from "typescript";
 
 // 계좌관리
 
@@ -6,27 +7,54 @@ import { useState } from "react";
 // 입력박스에는 입출금 금액 넣음.
 // 버튼: 입금버튼, 출금버튼
 
+const ListItem = ({ key, history }: { key: number; history: number }) => {
+  const color = history < 0 ? "blue" : "red";
+  return (
+    <li key={key} style={{ color: color }}>
+      {history}
+    </li>
+  );
+};
+
 const AccountManager = () => {
+  const [history, setHistory] = useState([]);
+  const deposit = () => {
+    const dep = parseInt(prompt("입금하실 금액을 입력해주세요.", 10));
+
+    setHistory([dep, ...history]);
+  };
+  const withdraw = () => {
+    prompt("출금하실 금액을 입력해주세요.");
+    return -prompt;
+  };
+
+  const sum = () => {
+    const bal = `${deposit}` + `${withdraw}`;
+  };
+  const [balance, bal] = useState(0);
+
   return (
     <div>
       <button
         onClick={() => {
-          prompt("금액을 입력해주세요.", "");
+          deposit();
         }}
       >
         입금하기
       </button>
       <button
         onClick={() => {
-          prompt("금액을 입력해주세요.", "");
+          withdraw();
         }}
       >
         출금하기
       </button>
-      <div>잔액</div>
+      <div>잔액: {balance}</div>
       <div>
         <ul>
-          <li></li>
+          {history.map((num, index) => (
+            <ListItem key={index} history={num} />
+          ))}
         </ul>
       </div>
     </div>
